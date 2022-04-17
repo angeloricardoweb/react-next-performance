9d984584113b7cd0ba0bc8bb809215ea0a8e295c
Angelo Ricardo <angeloengcomp@gmail.com>
2022-04-17 17:58:37 -0300

    ✨ NEW FEATURE: memoization

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ O Objetivo deste repositório e mostrar tecnicas de melhorias de performance em 
 ## Começando
 
 Instale as dependências
+
 ```bash
-npm install 
+npm install
 # ou
 yarn
 ```
@@ -22,6 +23,7 @@ yarn server
 O server ficará disponível em [http://localhost:3333/products](http://localhost:3333/products).
 
 Rode o app
+
 ```bash
 npm run dev
 # ou
@@ -30,4 +32,14 @@ yarn dev
 
 Abra [http://localhost:3000](http://localhost:3000) no seu navegador para ver o app.
 
+## Fluxo normal de renderização de um component react
 
+1. Criar uma nova versão do componente.
+2. Comparar com a versão anterior.
+3. Se houver diferenças, vai atualizar o que alterou.
+
+## Técnicas usadas
+
+### memo (ProductItem.tsx)
+
+- memoization: usado para evitar a renderização do componente se não houver mudanças.
--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,33 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.name} - <strong>{product.price}</strong>{" "}
     </div>
   );
 }
+
+/**
+ * Antes da primeira etapa de renderização (Criar uma nova versão do componente)
+ * compara produndamente o valor da propriedade anterior
+ * com o valor da nova propriedade,
+ * caso tenham ocorridos mudanças,
+ * ele renderediza novamente o componente,
+ * caso não,
+ * mantém o componente atual
+ */
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
+
+/**
+ * Quando usar
+ * 1. Componentes puros
+ * 2. Componentes que renderizam demais
+ * 3. Componentes médios pra grandes
+ */
